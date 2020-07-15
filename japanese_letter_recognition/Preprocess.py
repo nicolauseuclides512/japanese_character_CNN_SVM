92f6db6ab37a1936cde41a411ba6320770699f50
Nicolaus Euclides <nico.euclid@gmail.com>
2020-07-15 17:13:56 +0700

    add morphological proces for image processing

--- japanese_letter_recognition/Preprocess.py
+++ japanese_letter_recognition/Preprocess.py
@@ -1,18 +1,26 @@
 import cv2
 import os
 import glob
+import numpy as np
+
 import japanese_letter_recognition.FileNameList as listfname
 
 
 def bigPreprocess(main_directory, directoryname, files):
     data = []
-    dsize = (42, 42)
+    kernel = np.ones((3, 3), np.uint8)
+    kernel_morph = np.ones((2, 2), np.uint8)
     for f1 in files:
         img = cv2.imread(f1)
         grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
-        new_image = cv2.resize(blackAndWhiteImage, dsize)
-        data.append(new_image)
+        img_resize = cv2.resize(blackAndWhiteImage, (240, 240))
+        img_erotion = cv2.erode(img_resize, kernel, iterations=2)
+        img_morph_open = cv2.morphologyEx(img_erotion, cv2.MORPH_OPEN, kernel_morph, iterations=3)
+        img_morph_close = cv2.morphologyEx(img_morph_open, cv2.MORPH_CLOSE, kernel_morph, iterations=3)
+        img_dilation = cv2.dilate(img_morph_close, kernel, iterations=1)
+        img_resize = cv2.resize(img_dilation, (30, 30))
+        data.append(img_resize)
     if "Hiragana" in directoryname:
         os.chdir(main_directory + 'blackwhite_image/Hiragana')
     elif "Katakana" in directoryname:
