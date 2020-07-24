d2920fc8e4224fd9bf2a44507ff0445d1eb5d90b
nicolauseuclides512 <nico.euclid@gmail.com>
2020-07-24 15:22:06 +0700

    1. Fix file name list 2. Fix main program 3. Fix csv save file 4. try to program split data

--- japanese_letter_recognition/NeuralNetwork.py
+++ japanese_letter_recognition/NeuralNetwork.py
@@ -2,3 +2,24 @@ import tensorflow as tf
 
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
+
+letter_data = tf.data.experimental.CsvDataset(
+    '/home/nicolaus/PycharmProjects/thesis_program/japanese_letter_recognition/Kana_Letter.csv',
+    record_defaults=[tf.string, tf.string],
+    select_cols=[0, 1],
+    field_delim=",",
+    header=True
+)
+
+for data in letter_data:
+    tf.print(data)  # Print : {'Name': [Wii Sports], 'Platform': [Wii], 'Year': [2006]}
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(168, 168, 3)))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(10))
+# model.summary()
--- japanese_letter_recognition/Preprocess.py
+++ japanese_letter_recognition/Preprocess.py
@@ -3,7 +3,6 @@ import os
 import glob
 import numpy as np
 import csv
-
 import japanese_letter_recognition.FileNameList as listfname
 
 
@@ -13,20 +12,24 @@ def bigPreprocess(main_directory, directoryname, files):
     kernel_morph = np.ones((2, 2), np.uint8)
     for f1 in files:
         img = cv2.imread(f1)
-        grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_resize = cv2.resize(img, (168, 168))
+        grayImage = cv2.cvtColor(img_resize, cv2.COLOR_BGR2GRAY)
         (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
-        img_resize = cv2.resize(blackAndWhiteImage, (240, 240))
-        img_erotion = cv2.erode(img_resize, kernel, iterations=2)
+        img_erotion = cv2.erode(blackAndWhiteImage, kernel, iterations=2)
         img_morph_open = cv2.morphologyEx(img_erotion, cv2.MORPH_OPEN, kernel_morph, iterations=3)
         img_morph_close = cv2.morphologyEx(img_morph_open, cv2.MORPH_CLOSE, kernel_morph, iterations=3)
         img_dilation = cv2.dilate(img_morph_close, kernel, iterations=1)
-        img_resize = cv2.resize(img_dilation, (30, 30))
-        data.append(img_resize)
+        data.append(img_dilation)
     if "Hiragana" in directoryname:
         os.chdir(main_directory + 'blackwhite_image/Hiragana')
+        label = listfname.hiragana_label
+        checkDirectoryForSave(directoryname, data)
+        saveCSVFile(data, directoryname, label, "Hiragana")
     elif "Katakana" in directoryname:
         os.chdir(main_directory + 'blackwhite_image/Katakana')
-    checkDirectoryForSave(directoryname, data)
+        label = listfname.katakana_label
+        checkDirectoryForSave(directoryname, data)
+        saveCSVFile(data, directoryname, label, "Katakana")
     print(directoryname + ' done')
 
 
@@ -44,7 +47,6 @@ def saveImage(data, filename):
     for f2 in range(0, len(data)):
         if os.path.exists('{}'.format(filename + '_' + format(f2 + 1) + '.jpg', data[f2])) == 0:
             cv2.imwrite(filename + '_' + format(f2 + 1) + '.jpg', data[f2])
-            saveCSVFile(filename, data[f2])
 
 
 def hiraganaPreprocess(main_directory, directory_name):
@@ -65,8 +67,22 @@ def katakanaPreprocess(main_directory, directory_name):
         bigPreprocess(main_directory, img_dir, files)
 
 
-def saveCSVFile(file_name, data_array):
-    save_data = data_array.tolist()
-    with open(file_name + '.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerows(save_data)
+def saveCSVFile(data, filename, label, fname):
+    os.chdir('/home/nicolaus/PycharmProjects/thesis_program/japanese_letter_recognition')
+    data_array = []
+    for lname in label:
+        for f2 in range(0, len(data)):
+            if lname[1] == filename.partition("_")[2]:
+                data_array.append([
+                    '/home/nicolaus/PycharmProjects/thesis_program/blackwhite_image/' + fname + '/' + filename + '_' + format(
+                        f2 + 1) + '.jpg', lname[0]])
+    save_data = data_array
+    if os.path.exists('Kana_Letter.csv') == 0:
+        with open('Kana_Letter.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['data', 'label'])
+            writer.writerows(save_data)
+    else:
+        with open('Kana_Letter.csv', 'a+', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerows(save_data)
